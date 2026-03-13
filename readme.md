517175d805f4aff0a9ddb1955adafeadd9fc1420
akihikooooo <166023234+akihikooooo@users.noreply.github.com>
2026-03-14 00:16:34 +0800

    Removed vanilla HTML files lingering

--- readme.md
+++
@@ -1,5 +0,0 @@
-# Pearl Horizon Airlines Website
-## Introduction
-The sole purpose of this website is for users to book online using the website
-
-This website is made to fulfill the requirements for Applied Business Tools and Technology course of BSHM-1F
--- script.js
+++
@@ -1,9 +0,0 @@
-//Menu
-
-const menuToggle = document.querySelector('.menu-toggle');
-const navMenu = document.querySelector("#links ul")
-
-menuToggle.addEventListener("click", () => {
-    menuToggle.classList.toggle("toggled");
-    navMenu.classList.toggle("showed");
-})
