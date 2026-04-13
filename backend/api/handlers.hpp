1eb652afb8c553a2e84a48f935a1ae1534ac828a
bonny <128207489+bonaktan@users.noreply.github.com>
2026-04-14 06:51:53 +0800

    BUGGY: added processing of get url queries

--- backend/api/handlers.hpp
+++ backend/api/handlers.hpp
@@ -1,10 +1,11 @@
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 #include <boost/json.hpp>
-
+#include <boost/url.hpp>
+#include <boost/url/params_view.hpp>
 namespace beast = boost::beast;
 namespace http = beast::http;
-
+using namespace boost::urls;
 namespace APIHandlers {
-boost::json::value test(http::request<http::string_body> req);
+boost::json::value test(http::request<http::string_body> req, params_view params);
 }
--- backend/api/test.cpp
+++ backend/api/test.cpp
@@ -1,7 +1,12 @@
 #include "handlers.hpp"
 
-boost::json::value APIHandlers::test(http::request<http::string_body> req) {
-    return {{"status", "success"}, {"message", "this is a test"}};
+boost::json::value APIHandlers::test(http::request<http::string_body> req, params_view params) {
+
+    boost::json::value response = {{"status", "success"}, {"message", "this is a test"}};
+    for (auto param : params) {
+        response.as_object().emplace(param.key, param.key);
+    }
+    return response;
 }
 
 
