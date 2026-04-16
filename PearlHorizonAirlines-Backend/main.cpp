6669da11088bc235b5ccd4aa1fc37379082f6b84
bonny <128207489+bonaktan@users.noreply.github.com>
2026-04-16 18:45:26 +0800

    Added Listeners

--- PearlHorizonAirlines-Backend/main.cpp
+++ PearlHorizonAirlines-Backend/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <drogon/drogon.h>
+// #include <drogon/utils/logger.h>
+
 int main() {
     //Set HTTP listener address and port
-    drogon::app().addListener("192.168.1.5", 8080);
+    drogon::app().addListener("192.168.1.5", 8080).setLogPath("./logs").setLogLevel(trantor::Logger::kInfo);
     std::cout << "Running Backend" << std::endl;
     //Load config file
     //drogon::app().loadConfigFile("../config.json");
