f7103a597a9ba134bfb1263de27391e6645e3d4d
akihikooooo <166023234+akihikooooo@users.noreply.github.com>
2026-05-13 04:32:59 +0800

    Partial Admin refactoring

--- pearl_horizon/src/pages/Admin.jsx
+++ pearl_horizon/src/pages/Admin.jsx
@@ -1,4 +1,5 @@
-import "./stylesheets/account.css";
+// import "./stylesheets/account.css";
+import "./stylesheets/admin.css"
 import InputField from "../components/InputField";
 import { useEffect, useState } from "react";
 import axios from "axios";
@@ -8,6 +9,7 @@ const apiUrl = import.meta.env.VITE_BACKEND_URL;
 function AdminPanel() {
     const [dashboardData, setDashboardData] = useState({});
     const [loading, setLoading] = useState(true);
+    const [page, setPage] = useState("Dashboard");
     function newAirportSubmit(e) {
         e.preventDefault();
         const form = new FormData(e.target);
@@ -115,129 +117,25 @@ function AdminPanel() {
     }
     return (
         <>
-            <div id="cont" className="pt-16 flex bg-dusk-warm">
-                <div id="header">
-                    <h1 className="text-4xl text-sky-white flex justify-center items-center">Admin Panel</h1>
+            <div id="cont" className="pt-16 flex bg-sky-cloud">
+                <div id="sidebar" className="w-3/12 h-screen bg-sky-white p-6 flex flex-col gap-2">
+                    <h2 className="text-xl font-bold text-horizon">Admin Menu</h2>
+                    {["Dashboard", "Manage Airports", "Manage Airplane", "Manage Flight", "Manage Users"].map((item) => (
+                        <button
+                            key={item}
+                            onClick={() => {
+                                setPage(item);
+                            }}
+                            className={`text-horizon rounded-md p-2 hover:text-horizon-deep ${page === item ? "bg-horizon text-white" : ""}`}>
+                            {item}
+                        </button>
+                    ))}
                 </div>
-            </div>
-            <div id="stats">
-                <div className="flex gap-5 m-5">
-                    <div className="flex-1 h-32 bg-amber-100 rounded-lg shadow-md p-4">Total Users: {dashboardData.total_users}</div>
-                    <div className="flex-1 h-32 bg-amber-100 rounded-lg shadow-md p-4">Total Flights: {dashboardData.total_flights}</div>
-                    <div className="flex-1 h-32 bg-amber-100 rounded-lg shadow-md p-4">Total Booked Seats: {dashboardData.total_booked_seats}</div>
-                    <div className="flex-1 h-32 bg-amber-100 rounded-lg shadow-md p-4">
-                        Next Flight: {dashboardData.next_flight ? (
-                            <>
-                                <p>{dashboardData.next_flight.flight_id}</p>
-                                <p>
-                                    {new Intl.DateTimeFormat("en-US", {
-                                        dateStyle: "full",
-                                        timeStyle: "long",
-                                    }).format(dashboardData.next_flight.departure_timestamp * 1000)}
-                                </p>
-                            </>
-                    ) : <></>}
-                    </div>
-                    {/* <div className="flex-1 h-32 bg-amber-100 rounded-lg shadow-md p-4"></div> */}
-                </div>
-            </div>
-            <hr />
-            <div id="changes" className="m-5">
-                <div>
-                    <p class>Add Airports</p>
-                    <form onSubmit={newAirportSubmit} className="grid grid-cols-5 gap-2">
-                        <InputField label="Airport ID" name="airport_id" required placeholder="MNL" pattern="[A-Z]{3}" />
-                        <InputField label="Country" name="country" required placeholder="Philippines" />
-                        <InputField label="City" name="city" required placeholder="Manila" />
-                        <button type="submit">Submit</button>
-                    </form>
-                </div>
-                <div>
-                    <p class>Add Airplanes</p>
-                    <form onSubmit={newAirplaneSubmit} className="grid grid-cols-5 gap-2">
-                        <InputField
-                            label="Airplane ID"
-                            name="airplane_id"
-                            type="text"
-                            required
-                            pattern="PH[A-Z][0-9]{3}-[A-Z]"
-                            placeholder="PHA777-A"
-                        />
-                        <InputField label="Model" name="model" type="text" required placeholder="Boeing 777-300" />
-                        <InputField label="Seating Capacity" name="capacity" type="number" required min="1" placeholder="350" />
-                        <button type="submit">Submit</button>
-                    </form>
-                </div>
-                <div>
-                    <p class>Add Flights</p>
-                    <form onSubmit={newFlightSubmit} className="grid grid-cols-5 gap-2">
-                        <InputField label="Flight ID" name="flight_id" required pattern="PH[0-9]{4}" placeholder="PH0000" />
-                        <select name="origin_airport" required>
-                            <option value="" disabled selected>
-                                Select an Airport...
-                            </option>
-                        {Object.entries(dashboardData.airports_available).map(([airportID, data]) => (
-                            <option value={airportID}>
-                                {airportID} - {data.city}, {data.country}
-                            </option>
-                        ))}
-                        </select>
-                        <select name="destination_airport" required>
-                            <option value="" disabled selected>
-                                Select an Airport...
-                            </option>
-                            {Object.entries(dashboardData.airports_available).map(([airportID, data]) => (
-                                <option value={airportID}>
-                                    {airportID} - {data.city}, {data.country}
-                                </option>
-                            ))}
-                        </select>
-                        <select name="airplane_used" required>
-                            <option selected disabled value="">
-                                Select Airplane...
-                            </option>
-                            {Object.entries(dashboardData.airplanes_available).map(([airplaneID, data]) => (
-                                <option value={airplaneID}>
-                                    {data.model} - {data.seats} pax.
-                                </option>
-                            ))}
-                        </select>
-                        <select name="route_type" required>
-                            <option disabled selected value="">
-                                Select Route Type...
-                            </option>
-                            <option value="oneway">One Way</option>
-                            <option value="roundtrip">Round Trip</option>
-                        </select>
-                        <InputField label="Departure Time" name="departure_time" type="datetime-local" required min={dateNow()} />
-                        <InputField label="Flight Hour" name="flight_hour" type="number" required min="0" />
-                        <InputField label="Flight Minute" name="flight_minute" type="number" min="0" max="59" required />
-                        <button type="submit">Submit</button>
-                    </form>
-                </div>
-                <div>
-                    <p class>Modify User</p>
-                    <form onSubmit={modifyUserSubmit} className="grid grid-cols-5 gap-2">
-                        <select name="user">
-                            <option value="" disabled selected>
-                                Select a user...
-                            </option>
-                            {dashboardData.users.map((user, i) => (
-                                <option key={i} value={user.email}>
-                                    {user.name} ({user.email})
-                                </option>
-                            ))}
-                        </select>
-                        <select name="field">
-                            <option value="" disabled selected>Select a field...</option>
-                            <option value="first_name">First Name</option>
-                            <option value="last_name">Last Name</option>
-                            <option value="password">Password</option>
-                            <option value="permissions">Permissions</option>
-                        </select>
-                        <InputField label="Value" name="value" type="text" required/>
-                        <button type="submit">Submit</button>
-                    </form>
+                <div id="content" className="w-full overflow-scroll p-2">
+                    {page === "Dashboard" && <Dashboard dashboardData={dashboardData} />}
+                    {page === "Manage Airports" && <ManageAirport newAirportSubmit={newAirportSubmit} />}
+                    {page === "Manage Flight" && <AddFlight newFlightSubmit={newFlightSubmit} dashboardData={dashboardData} dateNow={dateNow} />}
+                    {page === "Manage Users" && <ModifyUser modifyUserSubmit={modifyUserSubmit} dashboardData={dashboardData} />}
                 </div>
             </div>
             <ToastContainer />
@@ -245,4 +143,148 @@ function AdminPanel() {
     );
 }
 
+function Dashboard({ dashboardData }) {
+    return (
+        <div id="stats">
+            <div className="flex gap-5 m-5">
+                <div className="flex-1 h-32 bg-amber-100 rounded-lg shadow-md p-4">Total Users: {dashboardData.total_users}</div>
+                <div className="flex-1 h-32 bg-amber-100 rounded-lg shadow-md p-4">Total Flights: {dashboardData.total_flights}</div>
+                <div className="flex-1 h-32 bg-amber-100 rounded-lg shadow-md p-4">Total Booked Seats: {dashboardData.total_booked_seats}</div>
+                <div className="flex-1 h-32 bg-amber-100 rounded-lg shadow-md p-4">
+                    Next Flight:{" "}
+                    {dashboardData.next_flight ? (
+                        <>
+                            <p>{dashboardData.next_flight.flight_id}</p>
+                            <p>
+                                {new Intl.DateTimeFormat("en-US", {
+                                    dateStyle: "full",
+                                    timeStyle: "long",
+                                }).format(dashboardData.next_flight.departure_timestamp * 1000)}
+                            </p>
+                        </>
+                    ) : (
+                        <></>
+                    )}
+                </div>
+                {/* <div className="flex-1 h-32 bg-amber-100 rounded-lg shadow-md p-4"></div> */}
+            </div>
+        </div>
+    );
+}
+
+function ManageAirport(newAirportSubmit){
+    return(
+        <div>
+            <div id="topbar" className="flex items-center justify-end p-4">
+                {/* <input type="text" placeholder="Search airports..." className="m-2"/> */}
+                    <button className="bg-green-600 text-white p-2 rounded-md">Add Flight</button>
+                </div>
+            <div id="table" className="w-full">
+                    <div className="table-row table-heading">
+                        <div className="table-cell">Airport ID</div>
+                        <div className="table-cell">Country</div>
+                        <div className="table-cell">City</div>
+                        <div className="table-cell"></div>
+                    </div>
+                    <div className="table-row">
+                        <div className="table-cell">MNL</div>
+                        <div className="table-cell">Philippines</div>
+                        <div className="table-cell">Manila</div>
+                        <div className="table-cell"><button className="flex justify-center items-center text-white bg-red-500 p-2 rounded-md"><span className="material-symbols-outlined">delete</span>Delete</button></div>
+                    </div>
+                </div>
+            </div>
+    )
+}
+function AddAirport({ newAirportSubmit }) {
+    return (
+        <div>
+            <p class>Add Airports</p>
+            <form onSubmit={newAirportSubmit} className="grid grid-cols-5 gap-2">
+                <InputField label="Airport ID" name="airport_id" required placeholder="MNL" pattern="[A-Z]{3}" />
+                <InputField label="Country" name="country" required placeholder="Philippines" />
+                <InputField label="City" name="city" required placeholder="Manila" />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+}
+function AddFlight({ newFlightSubmit, dashboardData, dateNow }) {
+    return (
+        <div>
+            <p class>Add Flights</p>
+            <form onSubmit={newFlightSubmit} className="grid grid-cols-5 gap-2">
+                <InputField label="Flight ID" name="flight_id" required pattern="PH[0-9]{4}" placeholder="PH0000" />
+                <select name="origin_airport" required>
+                    <option value="" disabled selected>
+                        Select an Airport...
+                    </option>
+                    {Object.entries(dashboardData.airports_available).map(([airportID, data]) => (
+                        <option value={airportID}>
+                            {airportID} - {data.city}, {data.country}
+                        </option>
+                    ))}
+                </select>
+                <select name="destination_airport" required>
+                    <option value="" disabled selected>
+                        Select an Airport...
+                    </option>
+                    {Object.entries(dashboardData.airports_available).map(([airportID, data]) => (
+                        <option value={airportID}>
+                            {airportID} - {data.city}, {data.country}
+                        </option>
+                    ))}
+                </select>
+                <select name="airplane_used" required>
+                    <option selected disabled value="">
+                        Select Airplane...
+                    </option>
+                    {Object.entries(dashboardData.airplanes_available).map(([airplaneID, data]) => (
+                        <option value={airplaneID}>
+                            {data.model} - {data.seats} pax.
+                        </option>
+                    ))}
+                </select>
+                <select name="route_type" required>
+                    <option disabled selected value="">
+                        Select Route Type...
+                    </option>
+                    <option value="oneway">One Way</option>
+                    <option value="roundtrip">Round Trip</option>
+                </select>
+                <InputField label="Departure Time" name="departure_time" type="datetime-local" required min={dateNow()} />
+                <InputField label="Flight Hour" name="flight_hour" type="number" required min="0" />
+                <InputField label="Flight Minute" name="flight_minute" type="number" min="0" max="59" required />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
+}
+function ModifyUser({ modifyUserSubmit, dashboardData }) {
+    return (
+        <div>
+         <p class>Modify User</p>
+         <form onSubmit={modifyUserSubmit} className="grid grid-cols-5 gap-2">
+             <select name="user">
+                 <option value="" disabled selected>
+                     Select a user...
+                 </option>
+                 {dashboardData.users.map((user, i) => (
+                     <option key={i} value={user.email}>
+                         {user.name} ({user.email})
+                     </option>
+                 ))}
+             </select>
+             <select name="field">
+                 <option value="" disabled selected>Select a field...</option>
+                 <option value="first_name">First Name</option>
+                 <option value="last_name">Last Name</option>
+                 <option value="password">Password</option>
+                 <option value="permissions">Permissions</option>
+             </select>
+             <InputField label="Value" name="value" type="text" required/>
+             <button type="submit">Submit</button>
+         </form>
+     </div>
+)}
 export default AdminPanel;
--- pearl_horizon/src/pages/booking/Payment.jsx
+++ pearl_horizon/src/pages/booking/Payment.jsx
@@ -1,5 +1,7 @@
 import InputField from "../../components/InputField";
+import {useState} from "react";
 import "../stylesheets/payment.css";
+import GCash from "../../assets/GCASH.JPG";
 
 function TextFields({ styles, start, middle, end }) {
     return (
@@ -16,11 +18,12 @@ function TextFields({ styles, start, middle, end }) {
 }
 
 function PaymentView(){
-    
+
 }
 
 function Payment() {
 
+    const [mode, setMop] = useState();
     const travelqty = 2;
     const sandwichqty = 2;
     const drinksqty = 2;
@@ -69,19 +72,46 @@ function Payment() {
                 <button className="bg-horizon text-sky-white px-4 py-2 rounded-lg">
                     Proceed to Payment
                 </button>
+
+                <div className="bg-black w-full h-full absolute top-0 left-0 opacity-50 z-10"></div>
+                <div className="bg-white w-8/12 p-6 rounded-lg z-20 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                    <h1 className="text-2xl text-horizon-deep font-bold mb-4">Payment</h1>
+                    <div className="flex border border-sky-cloud mb-6 rounded-sm overflow-hidden">
+                        {["GCash", "Card"].map((type) => (
+                            <button
+                                key={type}
+                                onClick={() => setMop(type)}
+                                className={`flex-1 py-2.5 text-[0.8rem] tracking-wide transition-colors font-medium
+                      ${mode === type ? "bg-horizon text-white" : "text-sky-slate hover:text-sky-night bg-transparent"}`}>
+                                {type}
+                            </button>
+                        ))}
+                    </div>
+                    {/* GCASH */}
+                    <div>
+                        <form className={`flex flex-col gap-4 ${mode === "GCash" ? "flex" : "hidden"}`}>
+                            <div className="flex items-center justify-center gap-6 w-full pl-3 text-horizon-deep mt-2">
+                                <img src={GCash} alt="GCash" className="w-3/12" />
+                                <InputField label="GCash Receipt" type="file" placeholder="" required/>
+                            </div>
+                            <button onClick={{}}className="bg-horizon text-white px-4 py-2 rounded mt-4 hover:bg-horizon-dark transition-colors duration-300">Pay Now</button>
+                        </form>
+                    </div>
+                    {/* CARD */}
+                    <div>
+                        <form className={`flex flex-col gap-4 ${mode === "Card" ? "flex" : "hidden"}`}>
+                            <div className="flex gap-4">
+                                <InputField label="Card Number" type="text" placeholder="1234 5678 9012 3456" required/>
+                                <InputField label="Expiration Date" type="text" placeholder="MM/YY" required/>
+                                <InputField label="CVV" type="text" placeholder="123" required/>
+                            </div>
+                            <button onClick={{}}className="bg-horizon text-white px-4 py-2 rounded mt-4 hover:bg-horizon-dark transition-colors duration-300">Pay Now</button>
+                        </form>
+                    </div>
+                        </div>
             </div>
         </>
     );
 }
 
 export default Payment;
-
-{
-    /* Add your payment form or components here */
-}
-{
-    /* <InputField label="Card Number" type="text" placeholder="1234 5678 9012 3456" />
-<InputField label="Expiration Date" type="text" placeholder="MM/YY" />
-<InputField label="CVV" type="text" placeholder="123" />
-<button className="bg-horizon text-white px-4 py-2 rounded mt-4 hover:bg-horizon-dark transition-colors duration-300">Pay Now</button> */
-}
